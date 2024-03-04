770dfed123caa5683e2fa5355c6941fa9d3bf34d
mjeongriver <jennygh2@hanmail.net>
2024-03-04 23:43:40 +0900

    영속성 컨텍스트 (2)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -29,14 +29,25 @@ public class JpaMain {
 //                System.out.println("member.name = " + member.getName());
 //            }
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             //영속
             System.out.println("=== BEFORE ===");
             em.persist(member); //db에 저장되지 않은 상태
-            em.detach(member); //영속성 컨텍스트에서 분리
+            //em.detach(member); //영속성 컨텍스트에서 분리
             System.out.println("=== AFTER ===");
+
+
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            //동일성 보장
+            System.out.println("findMember1 == findMember2: " + (findMember1 == findMember2));
+
+            //select 쿼리가 나가지 않음, 1차 캐시에서 조회
+            //System.out.println(findMember.getId());
+            //System.out.println(findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
